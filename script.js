237b9f1124ecb1e8fc45b93dbed4dbed4801a1b1
Naveen Kumar <nvnkr09@gmail.com>
2023-12-03 21:26:08 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,53 +1,75 @@
+// Selecting DOM elements
 const introPage = document.querySelector(".intro-page");
 const weatherPage = document.querySelector(".weather-page");
 const fetchBtn = document.getElementById("fetchButton");
 
-// get Location fn
+// Event listener for the "Fetch Data" button
 fetchBtn.addEventListener("click", () => {
   getLocation();
 });
 
+// Function to get user's location
 const getLocation = () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, handleError);
   }
 };
 
+// Error handling function for location retrieval
+function handleError() {
+  alert("Failed to get Location. Please allow access and try again.");
+}
+
+// Function to display user's location and proceed to the weather page
 function showPosition(position) {
+  // Displaying coordinates on the console
   console.log(position);
-  //   proceeding to next page
+
+  // Hiding the intro page and showing the weather page
   introPage.style.display = "none";
   weatherPage.style.display = "block";
 
+  // Extracting latitude and longitude
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
 
+  // Displaying latitude and longitude on the page
   document.getElementById("Latitude").innerHTML = `Lat: ${latitude}`;
   document.getElementById("Logitude").innerHTML = `Long: ${longitude}`;
+
+  // Embedding Google Map with the user's location
   document.getElementById(
     "mapFrame"
-  ).src = `https://maps.google.com/maps?q=${position.coords.latitude}, ${position.coords.longitude}&z=15&output=embed`;
+  ).src = `https://maps.google.com/maps?q=${latitude}, ${longitude}&z=15&output=embed`;
 
+  // Fetching weather data based on coordinates
   fetchWeatherData(latitude, longitude);
 }
 
-// wheather api Functions
+// Function to fetch weather data from the OpenWeatherMap API
 async function fetchWeatherData(latitude, longitude) {
   const apiKey = "e06e8c3b8d58cac4abf9c97d74602c0c";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
   try {
+    // Making a GET request to the API
     const response = await fetch(apiUrl);
     const data = await response.json();
 
+    // Logging the API response
     console.log(data);
+
+    // Displaying weather data on the UI
     displayWeatherData(data);
   } catch (error) {
+    // Handling errors during API request
     console.log("Something went wrong");
   }
 }
 
+// Function to display weather data on the UI
 function displayWeatherData(data) {
+  // Selecting DOM elements for weather information
   const location = document.getElementById("location");
   const windspeed = document.getElementById("windspeed");
   const humidity = document.getElementById("humidity");
@@ -57,6 +79,7 @@ function displayWeatherData(data) {
   const uvIndex = document.getElementById("uvIndex");
   const feelsLike = document.getElementById("feelsLike");
 
+  // Displaying weather information on the UI
   location.innerHTML = `Location: ${data.name}`;
   windspeed.innerHTML = `Wind Speed: ${data.wind.speed}kmph`;
   humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
@@ -66,28 +89,32 @@ function displayWeatherData(data) {
   uvIndex.innerHTML = `UV Index: 500`;
   feelsLike.innerHTML = `Feels Like: ${data.main.feels_like}°`;
 
+  // Function to convert API time zone offset to GMT
   function timeZoneConverter(offsetInSeconds) {
     // Convert the offset to hours
     const offsetInHours = offsetInSeconds / 3600;
-  
+
     // Determine the sign (positive/negative) for formatting
     const sign = offsetInHours >= 0 ? '+' : '-';
-  
+
     // Extract the hours and minutes
     const hours = Math.abs(Math.floor(offsetInHours));
     const minutes = Math.abs(Math.floor((offsetInHours % 1) * 60));
-  
+
     // Format the GMT string
     const gmtString = `GMT${sign}${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
-  
+
     return gmtString;
   }
 
+  // Function to convert pressure from hPa to atm
   function hPaToAtm(hPa) {
     return Math.round(hPa / 1013.25);
   }
 
+  // Function to convert wind direction from degrees to compass direction
   function Direction(degree) {
+    // ... (your implementation for converting degrees to compass direction)
     if (degree == 0) {
       return "North";
     }
@@ -114,47 +141,3 @@ function displayWeatherData(data) {
     }
   }
 }
-// const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=25.6156255&lon=85.0752824&appid=4b60220cba083d2d563d4c4bc1ffed29`;
-// {
-//     "coord": {
-//         "lon": 85.0753,
-//         "lat": 25.6156
-//     },
-//     "weather": [
-//         {
-//             "id": 701,
-//             "main": "Mist",
-//             "description": "mist",
-//             "icon": "50n"
-//         }
-//     ],
-//     "base": "stations",
-//     "main": {
-//         "temp": 298.14,
-//         "feels_like": 298.86,
-//         "temp_min": 298.14,
-//         "temp_max": 298.14,
-//         "pressure": 1013,
-//         "humidity": 83
-//     },
-//     "visibility": 3500,
-//     "wind": {
-//         "speed": 0,
-//         "deg": 0
-//     },
-//     "clouds": {
-//         "all": 11
-//     },
-//     "dt": 1701605101,
-//     "sys": {
-//         "type": 1,
-//         "id": 9129,
-//         "country": "IN",
-//         "sunrise": 1701564607,
-//         "sunset": 1701602926
-//     },
-//     "timezone": 19800,
-//     "id": 1273581,
-//     "name": "Dinapore",
-//     "cod": 200
-// }
